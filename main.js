d86aeaa11626f390ea56bb879b41c4080bc8d426
“Ryo <ryo.otama@gmail.com>
2019-11-19 17:50:37 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const element = document.querySelector("#gsr");
+element.style.setProperty("transform", "scale(2)");
+element.style.setProperty("transform-origin", "left bottom");
+element.style.setProperty("translate", "-200px");
